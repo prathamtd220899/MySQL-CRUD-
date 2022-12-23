78ac75dfbc2b5b8290b09e1ec495a896f346502f
Prathamesh dhasade <prathameshdhasade2015@gmail.com>
2022-12-23 16:59:52 +0530

    mysql crud

---
+++ src/server/index.js
@@ -0,0 +1,81 @@
+import express from "express";
+import mysql from "mysql2";
+import cors from "cors";
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "Pratham@2208",
+  database: "bookstore",
+});
+
+//ALTER USER "root"@'localhost' IDENTIFIED WITH mysql_native_password BY 'Pratham@2208';
+
+app.get("/", (req, res) => {
+  res.send("hello world!");
+});
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.json(err);
+    }
+    return res.json(data);
+  });
+});
+
+app.post("/books", (req, res) => {
+  const q = "INSERT INTO books(`title`, `desc`, `price`, `cover`) VALUES (?)";
+
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
+app.delete("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q = " DELETE FROM books WHERE id = ? ";
+
+  db.query(q, [bookId], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
+app.put("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q =
+    "UPDATE books SET `title`= ?, `desc`= ?, `price`= ?, `cover`= ? WHERE id = ?";
+
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ];
+
+  db.query(q, [...values, bookId], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`server started on port ${port}`);
+});
